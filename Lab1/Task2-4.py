9145df67bd9231d757fd54a679062b38578fffbc
LipiZed <kometa1810@gmail.com>
2024-02-21 14:03:08 +0300

    Вторая задача второго задания

--- Lab1/Task2-4.py
+++ Lab1/Task2-4.py
@@ -5,6 +5,11 @@ def count_russian_chars(text):
       count += 1
   return count
 
+def latin_palindrom(text):
+    text = text.lower()
+    text = "".join(char for char in text if char.isalpha() and char.islower() and not char.isalpha())
+    return text == text[::-1]
+
 
 
 
