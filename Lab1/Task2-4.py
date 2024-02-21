c491fe863860b566f6ea63fbc31f5d555c60f465
LipiZed <kometa1810@gmail.com>
2024-02-21 14:08:28 +0300

    Третья задача второго задания, сделал через регулярное выражение, так проще и быстреe

--- Lab1/Task2-4.py
+++ Lab1/Task2-4.py
@@ -1,3 +1,6 @@
+import re
+
+
 def count_russian_chars(text):
   count = 0
   for char in text:
@@ -10,6 +13,8 @@ def latin_palindrom(text):
     text = "".join(char for char in text if char.isalpha() and char.islower() and not char.isalpha())
     return text == text[::-1]
 
-
+def find_dates(text):
+    date_regex = r"\d{1,2}\.\d{1,2}\.\d{4}"
+    return re.findall(date_regex, text)
 
 
