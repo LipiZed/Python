430d5fdfcda98d08814f4b08df507a4cddc502eb
LipiZed <kometa1810@gmail.com>
2024-02-21 19:53:29 +0300

    13 Задача

--- Lab1/Task11-14.py
+++ Lab1/Task11-14.py
@@ -1,24 +1,48 @@
+# Задача 1
 def sogl_glasn(text):
     glasn = 'аеёиоуыэюя'
     sogl = 'бвгджзйклмнпрстфхцчшщъь'
     text = text.lower()
     glasn_count = sum(1 for char in text if char in glasn)
     sogl_count = sum(1 for char in text if char in sogl)
-    return sogl_count-glasn_count
+    return sogl_count - glasn_count
+
 
 def sort_by_sogl_glasn(strings):
     return sorted(strings, key=sogl_glasn)
 
 
+# Задача 4
 def average_ascii_weight(text):
     total_weight = sum(ord(char) for char in text)
     return total_weight / len(text)
+
 
 def quadratic_deviation(text, reference_avg_weight):
     avg_weight = average_ascii_weight(text)
     return (avg_weight - reference_avg_weight) ** 2
 
+
 def sort_strings_by_deviation(strings):
     reference_avg_weight = average_ascii_weight(strings[0])
     return sorted(strings, key=lambda x: quadratic_deviation(x, reference_avg_weight))
+
+
+# Задача 7
+
+def count_sochet(text):
+    glasn = 'аеёиоуыэюя'
+    sogl = 'бвгджзйклмнпрстфхцчшщъь'
+    count = 0
+    max_count = 0
+    for i in range(len(text) - 1):
+        if (text[i] in glasn and text[i + 1] in sogl) or (text[i] in sogl and text[i + 1] in glasn):
+            count += 1
+    else:
+        max_count = count
+        count = 0
+    return max_count
+
+def sort_count_sochet(strings):
+    return sorted(strings, key=count_sochet)
 
