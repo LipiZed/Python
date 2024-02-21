44549dfb065485df25a88e5a9b06d374efc249bc
LipiZed <kometa1810@gmail.com>
2024-02-21 14:51:51 +0300

    Сделал 7 задачу, она почти такая же как 6, но добавил нормальную обработку ввода формата 1/3 и подобных

--- Lab1/Task6-8.py
+++ Lab1/Task6-8.py
@@ -14,3 +14,26 @@ def find_max_float(text):
     else:
         return None
 
+def find_min_rational(text):
+    numbers = []
+    current_number = ''
+    for char in text:
+        if char.isdigit() or char == '.' or (char == '-' and current_number == '') or char == '/':
+            current_number += char
+        elif current_number:
+            numbers.append(current_number)
+            current_number = ''
+    if current_number:
+        numbers.append(current_number)
+    rational_numbers = []
+    for num in numbers:
+        if '/' in num:
+            parts = num.split('/')
+            rational_numbers.append(float(parts[0]) / float(parts[1]))
+        elif '.' in num:
+            rational_numbers.append(float(num))
+    if rational_numbers:
+        return min(rational_numbers)
+    else:
+        return None
+
