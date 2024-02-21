64da5283b0a7ad987411275b6173ce7109d71b8d
LipiZed <kometa1810@gmail.com>
2024-02-21 19:41:17 +0300

    12 Задача, по началу вообще непонятная

--- Lab1/Task11-14.py
+++ Lab1/Task11-14.py
@@ -9,3 +9,16 @@ def sogl_glasn(text):
 def sort_by_sogl_glasn(strings):
     return sorted(strings, key=sogl_glasn)
 
+
+def average_ascii_weight(text):
+    total_weight = sum(ord(char) for char in text)
+    return total_weight / len(text)
+
+def quadratic_deviation(text, reference_avg_weight):
+    avg_weight = average_ascii_weight(text)
+    return (avg_weight - reference_avg_weight) ** 2
+
+def sort_strings_by_deviation(strings):
+    reference_avg_weight = average_ascii_weight(strings[0])
+    return sorted(strings, key=lambda x: quadratic_deviation(x, reference_avg_weight))
+
