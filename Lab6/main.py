3e281bdeffc44e5337274d77782a2f6fa3334990
LipiZed <kometa1810@gmail.com>
2024-03-27 20:47:58 +0300

    Тут и вывод данных из таблицы через select и выгрузка файла в json

---
+++ Lab6/main.py
@@ -0,0 +1,30 @@
+import sqlite3
+import xml.dom.minidom as minidom
+
+conn = sqlite3.connect('lab6.db')
+
+cursor = conn.cursor()
+
+cursor.execute('SELECT * FROM Hotel')
+result = cursor.fetchall()
+print(result)
+doc = minidom.Document()
+
+root = doc.createElement('root')
+doc.appendChild(root)
+
+for row in cursor.fetchall():
+
+    row_elem = doc.createElement('row')
+    root.appendChild(row_elem)
+
+    for col_index, col_value in enumerate(row):
+        col_elem = doc.createElement('col{}'.format(col_index))
+        row_elem.appendChild(col_elem)
+        text = doc.createTextNode(str(col_value))
+        col_elem.appendChild(text)
+
+with open('mytable.xml', 'w') as f:
+    f.write(doc.toprettyxml(indent='  '))
+
+conn.close()
