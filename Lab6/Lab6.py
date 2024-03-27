1a72e0b7d84733e432c3d6a891fd02942c2c1429
LipiZed <kometa1810@gmail.com>
2024-03-27 17:56:04 +0300

    Создание CGI сервера

--- Lab6/Lab6.py
+++ Lab6/Lab6.py
@@ -1,28 +1,20 @@
-import sqlite3 as sq
+import http.server
+import sqlite3
 
-with sq.connect("lab6.db") as con:
-    cur = con.cursor()
-    cur.execute('''SELECT Hotel.name, COUNT(Rooms.id) AS room_count
-    FROM Hotel
-    LEFT JOIN Rooms ON Hotel.id = Rooms.hotel_id
-    GROUP BY Hotel.id;
-    ''')
-    result = cur.fetchall()
-    print(result)
-    cur.execute('''SELECT Rooms.*
-    FROM Rooms
-    LEFT JOIN Bookings ON Rooms.id = Bookings.room_id
-    WHERE Bookings.check_in NOT BETWEEN '2023-03-01' AND '2023-03-10'
-    AND Bookings.check_out NOT BETWEEN '2023-03-01' AND '2023-03-10'
-    OR Bookings.id IS NULL;
-    ''')
-    result = cur.fetchall()
-    print(result)
-    cur.execute('''SELECT DISTINCT guest_name, guest_phone, guest_email
-    FROM Bookings
-    JOIN Rooms ON Bookings.room_id = Rooms.id
-    JOIN Hotel ON Rooms.hotel_id = Hotel.id
-    WHERE Hotel.name = 'Отель Россия';
-    ''')
-    result = cur.fetchall()
-    print(result)
+
+class CGIHTTPRequestHandler(http.server.CGIHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        return super().do_GET()
+
+
+server_address = ('', 8000)
+httpd = http.server.HTTPServer(server_address, CGIHTTPRequestHandler)
+
+
+conn = sqlite3.connect('lab6.db')
+cursor = conn.cursor()
+
+print('Server started on http://localhost:8000')
+httpd.serve_forever()
